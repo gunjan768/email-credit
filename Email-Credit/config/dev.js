7797f5a28ac32cd5f134e2eef44c8712a999b941
Gunjan <57636775+gunjan768@users.noreply.github.com>
2020-06-19 22:31:03 +0530

    Create dev.js

---
+++ Email-Credit/config/dev.js
@@ -0,0 +1,11 @@
+module.exports = 
+{
+    googleClientID: "",
+    googleClientSecret: "",
+    mongoURI: "",
+    cookieKey: "",
+    stripePublishableKey: "",
+    stripeSecretKey: "",
+    sendGridKey: "",
+    redirectDomain: ""
+};
